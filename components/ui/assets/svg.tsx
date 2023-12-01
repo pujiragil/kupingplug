d35fe5409bcf9de468aef0b5fd8766c749af3cbc
pujiragil <ragilsg164@gmail.com>
2023-12-01 15:33:09 +0700

    added social media icon

--- components/ui/assets/svg.tsx
+++ components/ui/assets/svg.tsx
@@ -90,7 +90,6 @@ export const DeliveryIcon = ({ className }: { className?: string }) => {
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      -
       <path
         d="M10 12L6 12"
         stroke="#141718"
@@ -196,6 +195,82 @@ export const WishlistIcon = ({ className }: { className?: string }) => {
         clipRule="evenodd"
         d="M12.6924 6.91706C12.3055 7.28838 11.6945 7.28838 11.3076 6.91706L10.6152 6.2526C9.80477 5.47487 8.70994 5 7.5 5C5.01472 5 3 7.01472 3 9.5C3 11.8826 4.28979 13.8501 6.15176 15.4666C8.01532 17.0844 10.2434 18.1574 11.5746 18.7051C11.853 18.8196 12.147 18.8196 12.4254 18.7051C13.7566 18.1574 15.9847 17.0844 17.8482 15.4666C19.7102 13.85 21 11.8826 21 9.5C21 7.01472 18.9853 5 16.5 5C15.2901 5 14.1952 5.47487 13.3848 6.2526L12.6924 6.91706ZM12 4.80957C10.8321 3.6888 9.24649 3 7.5 3C3.91015 3 1 5.91015 1 9.5C1 15.8683 7.97034 19.385 10.8138 20.5547C11.5796 20.8697 12.4204 20.8697 13.1862 20.5547C16.0297 19.385 23 15.8682 23 9.5C23 5.91015 20.0899 3 16.5 3C14.7535 3 13.1679 3.6888 12 4.80957Z"
         fill="#141718"
+      />
+    </svg>
+  );
+};
+
+// social media icon
+export const InstagramIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      className={cn(className)}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="2"
+        y="2"
+        width="20"
+        height="20"
+        rx="4"
+        stroke="#141718"
+        strokeWidth="1.5"
+      />
+      <circle cx="18" cy="6" r="1" fill="#141718" />
+      <circle cx="12" cy="12" r="5" stroke="#141718" strokeWidth="1.5" />
+    </svg>
+  );
+};
+
+export const FacebookIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      className={cn(className)}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18 3H15C12.2386 3 10 5.23858 10 8V10H6V14H10V21H14V14H18V10H14V8C14 7.44772 14.4477 7 15 7H18V3Z"
+        stroke="#141718"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const YoutubeIcon = ({ className }: { className: string }) => {
+  return (
+    <svg
+      className={cn(className)}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="2"
+        y="3"
+        width="20"
+        height="18"
+        rx="4"
+        stroke="#141718"
+        strokeWidth="1.5"
+      />
+      <path
+        d="M10.4472 8.72361L15.2111 11.1056C15.9482 11.4741 15.9482 12.5259 15.2111 12.8944L10.4472 15.2764C9.78231 15.6088 9 15.1253 9 14.382V9.61803C9 8.87465 9.78231 8.39116 10.4472 8.72361Z"
+        stroke="#141718"
+        strokeWidth="1.5"
+        strokeLinecap="round"
       />
     </svg>
   );
