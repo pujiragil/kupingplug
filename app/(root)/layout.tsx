b23e9f4fa955d2e6c90e40519a2069dd11952e61
pujiragil <ragilsg164@gmail.com>
2023-12-03 10:28:38 +0700

    replace with single root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,22 +1,6 @@
 import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
 
-import { cn } from "@/utils/cn";
 import PageLayout from "@/layouts/pageLayout";
-
-import "../globals.css";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["500"],
-  variable: "--font-poppins",
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-inter",
-});
 
 export const metadata: Metadata = {
   title: "Kupingplug - Sumpel kuping untuk anda dan keluarga",
@@ -28,11 +12,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={cn(inter.variable, poppins.variable)}>
-      <body>
-        <PageLayout root={true}>{children}</PageLayout>
-      </body>
-    </html>
-  );
+  return <PageLayout root={true}>{children}</PageLayout>;
 }
---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+
+import { cn } from "@/utils/cn";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["500"],
+  variable: "--font-poppins",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Kupingplug - Sumpel kuping untuk anda dan keluarga",
+  description: "Kupingplug - Sumpel kuping untuk anda dan keluarga",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={cn(inter.variable, poppins.variable)}>
+      <body>{children}</body>
+    </html>
+  );
+}
