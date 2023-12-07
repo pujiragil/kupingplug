8dc8dea40be6c2a531f421ce428da880c5608beb
pujiragil <ragilsg164@gmail.com>
2023-12-07 20:32:25 +0700

    create Heading component with cva

---
+++ components/ui/head/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { cn } from "@/utils/cn";
+import { VariantProps, cva } from "class-variance-authority";
+
+type HeadingVariantsProps = VariantProps<typeof headingVariants>;
+
+const headingVariants = cva("font-poppins font-medium text-black/90", {
+  variants: {
+    intent: {
+      "hero-section": "text-[40px] md:text-6xl xl:text-7xl",
+      "base-section": "text-[34px] md:text-[40px]",
+    },
+  },
+  defaultVariants: {
+    intent: "hero-section",
+  },
+});
+
+export interface HeadingProps
+  extends React.HTMLAttributes<HTMLHeadingElement>,
+    HeadingVariantsProps {
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+}
+
+const Heading: React.FC<HeadingProps> = ({
+  as = "h1",
+  intent,
+  className,
+  ...props
+}) => {
+  return React.createElement(
+    as,
+    {
+      className: cn(headingVariants({ intent, className })),
+      ...props,
+    },
+    props.children,
+  );
+};
+
+export default Heading;
