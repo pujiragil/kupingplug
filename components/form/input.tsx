9433f418fae0b8b6975d3068184942589526268d
pujiragil <ragilsg164@gmail.com>
2023-12-13 13:10:05 +0700

    create Input component

---
+++ components/form/input.tsx
@@ -0,0 +1,34 @@
+//package
+import { type VariantProps, cva } from "class-variance-authority";
+
+// utils
+import { cn } from "@/utils/cn";
+
+type InputVariantsProps = VariantProps<typeof inputVariants>;
+
+const inputVariants = cva(
+  "font-inter text-base font-normal text-[#6C7275] outline-none placeholder:text-[#6C7275] placeholder:opacity-100 focus:text-[#141718]",
+  {
+    variants: {
+      intent: {
+        primary: "rounded-md border border-[#CBCBCB] px-4 py-2",
+        secondary: "pb-1.5",
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+    },
+  },
+);
+
+interface InputProps
+  extends InputVariantsProps,
+    React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input: React.FC<InputProps> = ({ intent, className, ...props }) => {
+  return (
+    <input className={cn(inputVariants({ intent, className }))} {...props} />
+  );
+};
+
+export default Input;
