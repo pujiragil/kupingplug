abf1c8038b43f709bf7915be4a1a06d7dfaa4ebb
pujiragil <ragilsg164@gmail.com>
2023-12-24 22:59:40 +0700

    remove test page

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-  return (
-    <div>
-      <div className="sticky top-0 h-[40px] w-full bg-green-400"></div>
-      <div className="h-screen w-full bg-yellow-400"></div>
-      <div className="h-screen w-full bg-purple-400"></div>
-    </div>
-  );
-}
