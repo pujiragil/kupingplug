04fd006e2b9df49f211b92b4652ee0cc1097085e
pujiragil <ragilsg164@gmail.com>
2023-12-24 22:51:44 +0700

    add description

--- lib/utils.ts
+++ lib/utils.ts
@@ -21,6 +21,9 @@ export const formatCurrency = (
   }).format(amount);
 };
 
-export const formatRating = (rating: number) => {
-  return Array.from({ length: rating >= 5 ? 5 : rating }, (_, idx) => idx + 1);
+export const formatRating = (ratings: number) => {
+  return Array.from(
+    { length: ratings >= 5 ? 5 : ratings },
+    (_, idx) => idx + 1,
+  );
 };
