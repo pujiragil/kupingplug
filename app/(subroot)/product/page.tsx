0abd809027b0548c4f7350bb5b0074c8bc3b2461
pujiragil <ragilsg164@gmail.com>
2024-01-03 23:23:04 +0700

    dummy review section

--- app/(subroot)/product/page.tsx
+++ app/(subroot)/product/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
             <ProductSlider />
           </div>
 
-          <div className="mx-auto max-w-[420px] lg:max-w-none">
+          <div className="mx-auto max-w-[420px] md:max-w-[520px] lg:max-w-none">
             <div className="space-y-4 border-b border-[#E8ECEF] pb-6">
               <div className="flex items-center gap-2.5">
                 <div className="flex items-center gap-1">
@@ -158,7 +158,7 @@ export default function Page() {
 
             <div className="space-y-4 border-b border-[#E8ECEF] py-6 lg:hidden">
               <div className="flex h-10 gap-2 lg:h-[52px]">
-                <div className="flex h-full w-1/2 items-center justify-between rounded bg-[#F5F5F5] px-2 md:w-2/5 lg:px-4">
+                <div className="flex h-full w-1/2 items-center justify-between rounded bg-[#F5F5F5] px-2 md:w-3/5 lg:px-4">
                   <MinusIcon
                     stroke="#141718"
                     className="h-4 w-4 lg:h-5 lg:w-6"
@@ -250,7 +250,7 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="mx-auto max-w-[420px] space-y-10 lg:max-w-none">
+        <div className="mx-auto max-w-[420px] space-y-10 md:max-w-[520px] lg:max-w-none">
           <div className="flex flex-col lg:flex-row lg:gap-10">
             <div className="flex items-center justify-between border-b border-[#6C7275] pb-2 lg:border-none lg:p-0">
               <p className="font-inter text-lg font-medium text-[#6C7275]">
@@ -274,7 +274,184 @@ export default function Page() {
               />
             </div>
           </div>
-          <div></div>
+          <div className="space-y-10">
+            <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between">
+              <p className="font-poppins text-[28px] font-medium text-[#141718]">
+                11 Reviews
+              </p>
+
+              <div className="flex items-center justify-between rounded-lg border-2 border-[#E8ECEF] p-2 pl-4  lg:w-64">
+                <p className="font-inter text-base font-semibold text-[#141718]">
+                  Newest
+                </p>
+                <DropdownIcon stroke="#6C7275" className="h-6 w-6" />
+              </div>
+            </div>
+
+            <div className="space-y-6">
+              <div className="space-y-4 border-b border-[#E8ECEF] pb-4">
+                <div className="flex items-center gap-4">
+                  <Image
+                    width={72}
+                    height={72}
+                    src="/images/avatar.png"
+                    alt="avatar"
+                    className="h-[72px] w-[72px] rounded-full object-cover object-center"
+                  />
+
+                  <div className="space-y-4">
+                    <p className="font-inter text-xl font-semibold text-[#141718]">
+                      Nicolas Jensen
+                    </p>
+                    <div className="flex items-center gap-1">
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                    </div>
+                  </div>
+                </div>
+                <p className="font-inter text-base font-normal text-[#353945] lg:pl-[88px]">
+                  I bought it 3 weeks ago and now come back just to say “Awesome
+                  Product”. I really enjoy it. At vero eos et accusamus et iusto
+                  odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                  deleniti atque corrupt et quas molestias excepturi sint non
+                  provident.
+                </p>
+              </div>
+              <div className="space-y-4 border-b border-[#E8ECEF] pb-4">
+                <div className="flex items-center gap-4">
+                  <Image
+                    width={72}
+                    height={72}
+                    src="/images/avatar.png"
+                    alt="avatar"
+                    className="h-[72px] w-[72px] rounded-full object-cover object-center"
+                  />
+
+                  <div className="space-y-4">
+                    <p className="font-inter text-xl font-semibold text-[#141718]">
+                      Nicolas Jensen
+                    </p>
+                    <div className="flex items-center gap-1">
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                    </div>
+                  </div>
+                </div>
+                <p className="font-inter text-base font-normal text-[#353945] lg:pl-[88px]">
+                  I bought it 3 weeks ago and now come back just to say “Awesome
+                  Product”. I really enjoy it. At vero eos et accusamus et iusto
+                  odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                  deleniti atque corrupt et quas molestias excepturi sint non
+                  provident.
+                </p>
+              </div>
+              <div className="space-y-4 border-b border-[#E8ECEF] pb-4">
+                <div className="flex items-center gap-4">
+                  <Image
+                    width={72}
+                    height={72}
+                    src="/images/avatar.png"
+                    alt="avatar"
+                    className="h-[72px] w-[72px] rounded-full object-cover object-center"
+                  />
+
+                  <div className="space-y-4">
+                    <p className="font-inter text-xl font-semibold text-[#141718]">
+                      Nicolas Jensen
+                    </p>
+                    <div className="flex items-center gap-1">
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                    </div>
+                  </div>
+                </div>
+                <p className="font-inter text-base font-normal text-[#353945] lg:pl-[88px]">
+                  I bought it 3 weeks ago and now come back just to say “Awesome
+                  Product”. I really enjoy it. At vero eos et accusamus et iusto
+                  odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                  deleniti atque corrupt et quas molestias excepturi sint non
+                  provident.
+                </p>
+              </div>
+              <div className="space-y-4 border-b border-[#E8ECEF] pb-4">
+                <div className="flex items-center gap-4">
+                  <Image
+                    width={72}
+                    height={72}
+                    src="/images/avatar.png"
+                    alt="avatar"
+                    className="h-[72px] w-[72px] rounded-full object-cover object-center"
+                  />
+
+                  <div className="space-y-4">
+                    <p className="font-inter text-xl font-semibold text-[#141718]">
+                      Nicolas Jensen
+                    </p>
+                    <div className="flex items-center gap-1">
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                    </div>
+                  </div>
+                </div>
+                <p className="font-inter text-base font-normal text-[#353945] lg:pl-[88px]">
+                  I bought it 3 weeks ago and now come back just to say “Awesome
+                  Product”. I really enjoy it. At vero eos et accusamus et iusto
+                  odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                  deleniti atque corrupt et quas molestias excepturi sint non
+                  provident.
+                </p>
+              </div>
+              <div className="space-y-4 border-b border-[#E8ECEF] pb-4">
+                <div className="flex items-center gap-4">
+                  <Image
+                    width={72}
+                    height={72}
+                    src="/images/avatar.png"
+                    alt="avatar"
+                    className="h-[72px] w-[72px] rounded-full object-cover object-center"
+                  />
+
+                  <div className="space-y-4">
+                    <p className="font-inter text-xl font-semibold text-[#141718]">
+                      Nicolas Jensen
+                    </p>
+                    <div className="flex items-center gap-1">
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                      <StarIcon fill="#141718" className="h-4 w-4" />
+                    </div>
+                  </div>
+                </div>
+                <p className="font-inter text-base font-normal text-[#353945] lg:pl-[88px]">
+                  I bought it 3 weeks ago and now come back just to say “Awesome
+                  Product”. I really enjoy it. At vero eos et accusamus et iusto
+                  odio dignissimos ducimus qui blanditiis praesentium voluptatum
+                  deleniti atque corrupt et quas molestias excepturi sint non
+                  provident.
+                </p>
+              </div>
+
+              <div className="flex justify-center">
+                <button className="rounded-full border border-[#141718] px-10 py-2 font-inter text-base font-medium text-[#141718]">
+                  Load more
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </SectionLayout>
