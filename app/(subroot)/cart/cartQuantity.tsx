ac9cc40fdd3c98d28e0069539959498375a77659
pujiragil <ragilsg164@gmail.com>
2024-01-26 23:34:17 +0700

    disabled cartQuantity option

--- app/(subroot)/cart/cartQuantity.tsx
+++ app/(subroot)/cart/cartQuantity.tsx
@@ -1,23 +1,36 @@
 // package
 import { Minus, Plus } from "lucide-react";
 
-const CartQuantity = ({
-  quantity,
-  onAddQuantity,
-  onMinusQuantity,
-}: {
+interface CartQuantityProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   quantity: number;
   onAddQuantity: () => void;
   onMinusQuantity: () => void;
+}
+
+const CartQuantity: React.FC<CartQuantityProps> = ({
+  quantity,
+  onAddQuantity,
+  onMinusQuantity,
+  ...props
 }) => {
   return (
-    <div className="flex w-[96px] items-center justify-between rounded border border-[#6C7275] p-2">
-      <Minus onClick={onMinusQuantity} className="h-4 w-4 cursor-pointer" />
+    <button
+      {...props}
+      className="group flex w-[96px] items-center justify-between rounded border border-[#6C7275] p-2"
+    >
+      <Minus
+        onClick={onMinusQuantity}
+        className="h-4 w-4 cursor-pointer group-disabled:cursor-not-allowed"
+      />
       <span className="font-inter text-xs font-semibold text-[#141718]">
         {quantity}
       </span>
-      <Plus onClick={onAddQuantity} className="h-4 w-4 cursor-pointer" />
-    </div>
+      <Plus
+        onClick={onAddQuantity}
+        className="h-4 w-4 cursor-pointer group-disabled:cursor-not-allowed"
+      />
+    </button>
   );
 };
 
